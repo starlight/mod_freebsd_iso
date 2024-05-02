db3bc41832b008bd21544d041c19ed5bd1f44615
Arizona Hanson <arizona@starlig.ht>
2024-05-02 17:14:39 -0400

    specify memory disc type

--- extract_iso.sh
+++ extract_iso.sh
@@ -6,7 +6,7 @@ if [ ! -x /usr/local/bin/rsync ]; then
   pkg install rsync || exit 1
 fi
 set -ex
-mdconfig -f "$1" -u md0
+mdconfig -t vnode -f "$1" -u md0
 MD_DIR="$(mktemp -d -p ./)"
 mount -t cd9660 /dev/md0 ${MD_DIR}
 rsync -a ${MD_DIR}/ "$2"
